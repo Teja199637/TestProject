af14c663a6913f2a69094c96b821a26b31822c6f
Ravi Teja Akshinthala <teja199537@gmail.com>
2018-06-21 19:33:01 +0530

    Third Commit

--- TestProject/src/org/sample/practice/examples/TestProgram.java
+++ TestProject/src/org/sample/practice/examples/TestProgram.java
@@ -3,7 +3,6 @@ package org.sample.practice.examples;
 public class TestProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Ravi Teja Akshinthala");
 	}
 
